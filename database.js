cb5c9e0208bb833acf2dbb8d6b0dd31a8e43d645
pavan <Hackerart512@gmail.com>
2024-05-24 12:40:17 +0530

    update

--- database.js
+++ database.js
@@ -6,24 +6,28 @@ const mogooseURI = process.env.MONGO_URL;
 
 mongoose.set('debug', true);
 
- 
-
 mongoose.connect(mogooseURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4
 })
-    .then(() => {
-        console.log('Successfully connected to MongoDB');
-    })
-    .catch(err => {
-        console.error('Error connecting to MongoDB:', err);
-    });
+.then(() => {
+  console.log('Successfully connected to MongoDB');
+})
+.catch(err => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 
-db.on('connected', function () {
-    console.log('Connected to MongoDB successfully...');
+db.on('connected', () => {
+  console.log('Connected to MongoDB successfully...');
+});
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
 });
 
 module.exports = db;
