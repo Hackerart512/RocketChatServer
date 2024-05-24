2a993c935458ba040f7168d98cc97a63fca316be
pavan <Hackerart512@gmail.com>
2024-05-24 12:18:39 +0530

    update

--- database.js
+++ database.js
@@ -17,7 +17,11 @@ var db = mongoose.connection;
 // const uri = 'mongodb+srv://hacker:hacker@cluster0.ltt9gpg.mongodb.net/RocketChat?retryWrites=true&w=majority';
 // const uri = 'mongodb+srv://hacker:hacker@cluster0.ltt9gpg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(mogooseURI, {})
+mongoose.connect(mogooseURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+})
    
 db.on('connected', function () {
     console.log('Connect to mongoose successfully...');
